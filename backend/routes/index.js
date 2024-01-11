b5f337ff379983aadfd1ebdc4e4ff72c9a63d5ee
Tyler_Han <tylerhan1226@gmail.com>
2024-01-11 09:43:19 -0800

    auth-me-2

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,8 +2,8 @@
 const express = require('express');
 const router = express.Router();
 
-// const apiRouter = require('./api');
-// router.use('/api', apiRouter);
+const apiRouter = require('./api');
+router.use('/api', apiRouter);
 
 //testing server
 // router.get('/hello/world', function(req, res) {
