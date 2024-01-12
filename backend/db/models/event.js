885cecb7be770df4795acc4114b0cd6d016b813e
Tyler_Han <tylerhan1226@gmail.com>
2024-01-11 17:30:37 -0800

    edit event description validation on model/migration

--- backend/db/models/event.js
+++ backend/db/models/event.js
@@ -33,7 +33,7 @@ module.exports = (sequelize, DataTypes) => {
     venueId: DataTypes.INTEGER,
     groupId: DataTypes.INTEGER,
     name: DataTypes.STRING,
-    description: DataTypes.STRING,
+    description: DataTypes.TEXT,
     type: DataTypes.STRING,
     capacity: DataTypes.INTEGER,
     price: DataTypes.FLOAT,
