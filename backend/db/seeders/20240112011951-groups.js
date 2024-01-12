3de1687098bf11fae4083d790b438f187b896368
Tyler_Han <tylerhan1226@gmail.com>
2024-01-11 17:27:49 -0800

    Edit groups seeder

--- backend/db/seeders/20240112011951-groups.js
+++ backend/db/seeders/20240112011951-groups.js
@@ -15,7 +15,7 @@ module.exports = {
     {
       organizerId: 1,
       name: 'EDM Squad',
-      about: "Welcome to EDM Squad, LA's hub for electronic music lovers! Immerse in the electrifying world of EDM, where bass drops and synths create unforgettable moments. Join us to amplify LA's electronic scene, uniting enthusiasts for epic memories and live events.",
+      about: "Welcome to EDM Squad",
       type: 'In person',
       private: false,
       city: 'Los Angeles',
@@ -24,7 +24,7 @@ module.exports = {
     {
       organizerId: 2,
       name: 'The Best 5',
-      about: "Welcome to The Best 5 - a community of passionate League of Legends players. Join us for weekend battles, where strategy, teamwork, and skill collide on the Summoner's Rift. Embark on a journey of epic clashes, unforgettable plays, and the sweet taste of victory in the ever-evolving world of League of Legends.",
+      about: "Welcome to The Best 5",
       type: 'Online',
       private: false,
       city: 'Cincinnati',
@@ -33,7 +33,7 @@ module.exports = {
     {
       organizerId: 3,
       name: "NYC Ballers' Expedition",
-      about: "Join NYC Ballers' Expedition, where we explore the best street basketball spots in NYC! Gather with fellow enthusiasts for weekend events, unwind, and enjoy the thrill of hoops. Let's turn NYC's concrete into our playground. Lace up, play hard, and have a ball!",
+      about: "Join NYC Ballers' Expedition",
       type: 'In person',
       private: false,
       city: 'New York City',
@@ -42,7 +42,7 @@ module.exports = {
     {
       organizerId: 4,
       name: 'Metalheads',
-      about: "Welcome to Metalheads, where the power of metal unites. Dive into diverse genres, form bands, and connect with fellow enthusiasts. Whether you're a player or fan, join us for epic jam nights, workshops, and gigs. Let's amplify the metal scene and forge our legacy together.",
+      about: "Welcome to Metalheads",
       type: 'In person',
       private: false,
       city: 'San Francisco',
@@ -51,7 +51,7 @@ module.exports = {
     {
       organizerId: 5,
       name: 'Architecture Explorers',
-      about: "Discover LA's architectural marvels with Architecture Explorers! Join our tours showcasing iconic designs and historical treasures. Immerse in the art and stories shaping the city's skyline. Let's explore together!",
+      about: "Discover LA's architectural marvels with Architecture Explorers!",
       type: 'In person',
       private: false,
       city: 'Los Angeles',
