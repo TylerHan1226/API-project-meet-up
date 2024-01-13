45a4a68ecb8e1f8402e350a1213352bc7216dc7d
Tyler_Han <tylerhan1226@gmail.com>
2024-01-12 17:01:28 -0800

    updated group routers 1,2,3,4

--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -27,67 +27,87 @@ router.get('/', async (req, res) => {
     //get groups
     const groups = await Group.findAll({
         include: [
-            { model: GroupImage, attributes: ['url'], where: {preview: true} },
+            { model: GroupImage, attributes: ['url', 'preview'] },
             { model: Membership, attributes: ['id', 'groupId'] }
         ]
     })
+
     //create result array
     const reGroups = []
     for (let eachGroup of groups) {
         reGroups.push(eachGroup.toJSON())
     }
     for (let eachRGroup of reGroups) {
-        eachRGroup.numMembers = eachRGroup.Memberships.length
+        //numMembers
+        eachRGroup.numMembers = 1
+        if (eachRGroup.Memberships.length !== 0) {
+            eachRGroup.numMembers = eachRGroup.Memberships.length
+        }
         delete eachRGroup.Memberships
-        eachRGroup.previewImage = eachRGroup.GroupImages[0].url
+        //previewImage
+        eachRGroup.previewImage = null
+        for (let eachImg of eachRGroup.GroupImages) {
+            if (eachImg.preview == true) {
+                const previewImage = eachImg.url
+                eachRGroup.previewImage = previewImage
+            }
+        }
         delete eachRGroup.GroupImages
     }
-    
-    return res.status(200).json({Groups: reGroups})
+
+    return res.status(200).json({ Groups: reGroups })
 })
 
 
 //Get all Groups joined or organized by the Current User
 router.get('/current', requireAuth, async (req, res) => {
-    
+
     const { user } = req
     const groups = await Group.findAll({
         include: [
-            { model: GroupImage, attributes: ['url'], where: {preview: true} },
-            { model: Membership, attributes: ['id', 'userId', 'groupId'] }
+            { model: GroupImage, attributes: ['url'], where: { preview: true } },
+            { model: Membership, attributes: ['id', 'userId', 'groupId'] }          
         ]
+    })
+    const groupAsOrganizer = await Group.findOne({
+        where: {organizerId: user.id}
     })
 
     const reGroups = []
+    groups.push(groupAsOrganizer)
     for (let eachGroup of groups) {
-        for (let eachMembership of eachGroup.Memberships) {
-            //check if the current user belongs to the membership
-            if (eachMembership.userId == user.id) {
-                reGroups.push(eachGroup.toJSON())
-            }
-        }
+        reGroups.push(eachGroup.toJSON())
     }
     for (let eachRGroup of reGroups) {
-        eachRGroup.numMembers = eachRGroup.Memberships.length
-        delete eachRGroup.Memberships
-        eachRGroup.previewImage = eachRGroup.GroupImages[0].url
-        delete eachRGroup.GroupImages
+        if (eachRGroup.Memberships) {
+            eachRGroup.numMembers = eachRGroup.Memberships.length
+            delete eachRGroup.Memberships
+        } else {
+            eachRGroup.numMembers = 1
+        }
+        if (eachRGroup.GroupImages) {
+            eachRGroup.previewImage = eachRGroup.GroupImages[0].url
+            delete eachRGroup.GroupImages
+        } else {
+            eachRGroup.previewImage = null
+        }
     }
     
-    return res.status(200).json({Groups: reGroups})
+    // return res.status(200).json(groups)
+    return res.status(200).json({ Groups: reGroups })
 })
 
 
 // Get details of a Group from an id
 router.get('/:groupId', async (req, res) => {
-    
+
     const { groupId } = req.params
     const group = await Group.findOne({
         where: { id: groupId },
         include: [
-            { model: GroupImage, attributes: ['id', 'url', 'preview']},
+            { model: GroupImage, attributes: ['id', 'url', 'preview'] },
             { model: Membership, attributes: ['id', 'groupId'] },
-            { model: User, as: 'Organizer', attributes: ['id', 'firstName', 'lastName']},
+            { model: User, as: 'Organizer', attributes: ['id', 'firstName', 'lastName'] },
             { model: Venue }
         ]
     })
@@ -97,7 +117,7 @@ router.get('/:groupId', async (req, res) => {
             "message": "Group couldn't be found"
         })
     }
-    
+
     const resultGroups = group.toJSON()
     resultGroups.numMembers = resultGroups.Memberships.length
     delete resultGroups.Memberships
@@ -112,10 +132,10 @@ router.get('/:groupId', async (req, res) => {
 router.post('/', requireAuth, groupInputsValidation.createGroup(), async (req, res) => {
 
     const { user } = req
-    const {name, about, type, private, city, state} = req.body
+    const { name, about, type, private, city, state } = req.body
     const organizerId = user.id
     //Build instance
-    const newGroup = Group.build({
+    const newGroup = await Group.build({
         organizerId, name, about, type, private, city, state
     })
     await newGroup.save()
