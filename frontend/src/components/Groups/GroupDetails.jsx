56453f1e09d879aa755e23ae28cbedd2562382a8
Tyler_Han <tylerhan1226@gmail.com>
2024-02-22 18:06:11 -0800

    fix run build error

--- frontend/src/components/Groups/GroupDetails.jsx
+++ frontend/src/components/Groups/GroupDetails.jsx
@@ -29,7 +29,7 @@ function GroupDetails() {
         if (!events) {
             dispatch(fetchEventsByGroupThunk(groupId))
         }
-    }, [dispatch, groupId])
+    }, [dispatch, groupId, events])
 
     let previewImgUrl = ''
     let isPublic = 'Public'
